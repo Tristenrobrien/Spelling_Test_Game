9403453a1893c52a5d59b201507503b0c3be6769
Tristen OBrien <35441158+Tristenrobrien@users.noreply.github.com>
2022-02-27 00:44:06 -0500

    Update Spelling_Quest.py

--- Spelling_Quest.py
+++ Spelling_Quest.py
@@ -1,7 +1,3 @@
-'''
-Lucy's Spelling Game
-
-'''
 
 import random
 import time
@@ -9,10 +5,13 @@ import webbrowser
 import sys
 import os
 
-         
+#Add which ever words are on the spelling test         
 wordList = ['about', "wouldn't", 'almost', 'laugh', 'especially', 'crown', 'however', 'powerful', 'allowed', 'fountain', 'thousand', 'around', 'ground']
+
+#This will allow words that were spelled correctly to be removed from the game so there aren't any duplicates
 completedWords = []
 
+#Encouragement when the word is spelled correctly.  This was made for my daughter
 youGo = ['Great Job!', 'You go girl!', 'Keep it up!', 'You are almost there!', 'Fantastic', 'You are so smart!', 'Way to go!']
 youGoRand = random.choice(youGo)
 
@@ -22,18 +21,19 @@ time.sleep(.5)
 print (input('\nPress the Enter key to start: '))
 
 print ('Here is how you play - you will be shown the first and last letter of your word.')
-#os.system("say 'Here is how you play - you will be shown the first and last letter of your word.'")
 
 print ('\nYou must spell all the words correctly to win the game!!!\n')
-#os.system("say 'You must spell all the words correctly to unlock win the game!'")
 
+
+#games starts
 time.sleep(.5)
 print(input('\nPress the Enter key for your first question: '))
 time.sleep(1)
 
 def game():
     if len(wordList) != 0:  
-
+         
+        #Random word is chosen, spoken, and printed on screen
         word = random.choice(wordList)
         dash = (len(word) - 2)
         question = (word[0:1] + ('_' * dash) + word[-1:])
@@ -48,7 +48,7 @@ def game():
             time.sleep(1.5)
             game()
 
-        
+        #If guessed incorrectly, it asks to try again
         else:
             print ('Try again')
             time.sleep(.5)
@@ -61,7 +61,8 @@ def game():
                 print ('\nOnly ' + str(len(wordList)) + ' more words to go!')
                 time.sleep(1.5)
                 game()
-                
+            
+            #If guessed incorrectly again, it will give hint   
             else:
                 if answer != word:
                     dash = (len(word) - 3)
